120d53b965217e64231e7ceefbef154cfaaf0b06
juks01 <juksun@gmail.com>
2024-11-08 20:56:56 +0200

    Separated rendmap from gamemap. Frame still not working.

--- DiggerTest2/main.c
+++ DiggerTest2/main.c
@@ -5,28 +5,82 @@
 #include <time.h>
 
 
-static int init(System* sys, Renderer* rend, Map* m, Player* plr) {
+static void gen_gamearea() {
+	int num_blocks = sizeof(blocks) / sizeof(blocks[0]);
+	for (int r = 0; r < G_AREA_ROWS; r++)
+		for (int c = 0; c < G_AREA_COLS; c++)
+			gamemap[r][c].block = blocks[random_number(0, num_blocks)];
+}
+
+static void gen_rendarea_borders(Renderer* rend) {
+	int start_x = rend->rendarea_start.X;
+	int start_y = rend->rendarea_start.Y;
+	int end_x = rend->rendarea_end.X;
+	int end_y = rend->rendarea_end.Y;
+	for (int r = start_y; r < end_y; r++)
+		for (int c = start_x; c < end_x; c++)
+			if (r == start_y)            // == The first row ==
+				if (c == start_x)        // Upper left corner
+					rendmap[r][c].block = walls[0];
+				else if (c == end_x -1) // Upper right corner
+					rendmap[r][c].block = walls[2];
+				else                     // Upper center characters
+					rendmap[r][c].block = walls[1];
+			else if (r == end_y - 1)     // == The last row ==
+				if (c == start_x)        // Lower left corner
+					rendmap[r][c].block = walls[3];
+				else if (c == end_x -1) // Lower right corner
+					rendmap[r][c].block = walls[5];
+				else                     // Lower center characters
+					rendmap[r][c].block = walls[4];
+}
+
+static void draw_rendarea_borders(Renderer* rend, HANDLE* hOut) {
+	SetConsoleCursorPosition(hOut, rend->rendarea_start);
+	int start_x = rend->rendarea_start.X;
+	int start_y = rend->rendarea_start.Y;
+	int end_x = rend->rendarea_end.X;
+	int end_y = rend->rendarea_end.Y;
+	for (int r = start_y; r < end_y; r++) {
+		for (int c = start_x; c < end_x; c++) {
+			printf("%c", rendmap[r][c].block.tile);
+		}
+		printf("\n");
+	}
+}
+
+static int init(System* sys, Renderer* rend, Player* plr) {
 	sys->debug = SYS_DEBUG;
 	sys->show_fps = 1;
 	sys->show_frames = 1;
 
+	rend->rendarea_start.X = 0;
+	rend->rendarea_start.Y = REN_HEADER_ROWS;
+	rend->rendarea_end.X = REN_COLS;
+	rend->rendarea_end.Y = REN_HEADER_ROWS + REN_ROWS;
 	rend->header_start.X = 0;
 	rend->header_start.Y = 0;
 	rend->header_end.X = REN_COLS;
 	rend->header_end.Y = REN_HEADER_ROWS;
-	rend->gamearea_start.X = 0;
-	rend->gamearea_start.Y = REN_HEADER_ROWS;
-	rend->gamearea_end.X = REN_COLS;
-	rend->gamearea_end.Y = REN_HEADER_ROWS + REN_ROWS;
+	rend->gamearea_start.X = rend->rendarea_start.X +1;
+	rend->gamearea_start.Y = rend->rendarea_start.Y +1;
+	rend->gamearea_end.X = rend->rendarea_end.X - 1;
+	rend->gamearea_end.Y = rend->rendarea_end.X - 1;
 	rend->statsarea_start.X = 0;
 	rend->statsarea_start.Y = REN_HEADER_ROWS + REN_ROWS;
 	rend->statsarea_end.X = REN_COLS;
 	rend->statsarea_end.Y = REN_HEADER_ROWS + REN_ROWS + REN_STATS_ROWS;
+	rend->upd_rendarea = 1;
 	rend->upd_header = 1;
 	rend->upd_gamearea = 1;
 	rend->upd_stats = 1;
 
 	plr->name = "Player";
+	plr->health = 0;
+	plr->atkbase = 0;
+	plr->atkboost = 0;
+	plr->defbase = 0;
+	plr->defboost = 0;
 	plr->dir = 9;
 	plr->marker = "@";
 	plr->Position.X = 10;
@@ -41,10 +95,13 @@ static int init(System* sys, Renderer* rend, Map* m, Player* plr) {
 	cur.bVisible = FALSE;
 	SetConsoleCursorInfo(hOut, &cur);
 
+	// Generate random map
+	gen_gamearea();
+	// Generate game area borders
+	gen_rendarea_borders(rend);
+
 	return 0;
 }
-
-
 
 static char input() {
 	if (_kbhit())
@@ -54,6 +111,8 @@ static char input() {
 }
 
 
+
+
 static int update() {
 
 	return 0;
@@ -61,8 +120,91 @@ static int update() {
 
 
 
-static int renderer() {
+static int rend_upd_header(System* sys, Renderer* rend, HANDLE hOut) {
+	if (sys->debug) {
+		// Debug texts
+		COORD cd = rend->header_start;
+		cd.X += 49;
+		SetConsoleCursorPosition(hOut, cd);
+		printf("H-DEBUG: 12345678901234567890");
+	}
+	SetConsoleCursorPosition(hOut, rend->header_start);
+	printf(" ---=== HEADER ROW ===--- ");
+	return 0;
+}
 
+static int rend_upd_gamearea(Renderer* rend, HANDLE hOut) {
+	int start_x = rend->rendarea_start.X;
+	int start_y = rend->rendarea_start.Y;
+	int end_x = rend->rendarea_end.X;
+	int end_y = rend->rendarea_end.Y;
+
+	// Print visible part of map
+	CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
+	WORD saved_attributes;
+	/* Save current attributes */
+	GetConsoleScreenBufferInfo(hOut, &consoleInfo);
+	saved_attributes = consoleInfo.wAttributes;
+
+	SetConsoleCursorPosition(hOut, rend->rendarea_start);
+	for (int r = start_y; r < end_y; r++) {
+		for (int c = start_x; c < end_x; c++) {
+			if (gamemap[r][c].block.tile == CHAR_GRASS) {
+				SetConsoleTextAttribute(hOut, FOREGROUND_GREEN);
+				printf("%c", gamemap[r][c].block.tile);
+				SetConsoleTextAttribute(hOut, saved_attributes);
+			}
+			else if (gamemap[r][c].block.tile == BL_BLOCKING) {
+				SetConsoleTextAttribute(hOut, FOREGROUND_INTENSITY);
+				printf("%c", gamemap[r][c].block.tile);
+				SetConsoleTextAttribute(hOut, saved_attributes);
+			}
+			else
+				printf("%c", gamemap[r][c].block.tile);
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
+static int upd_rend_gamearea_borders(Renderer* rend, HANDLE hOut) {
+	gen_rendarea_borders(rend);
+	draw_rendarea_borders(rend, hOut);
+	// Print the borders?
+	return 0;
+}
+
+
+static int rend_upd_stats(System* sys, Renderer* rend, Player* plr, HANDLE hOut) {
+	if (sys->debug) {
+		// Debug texts
+		COORD cd = rend->statsarea_start;
+		cd.X += 49;
+		SetConsoleCursorPosition(hOut, cd);
+		printf("S-DEBUG: 12345678901234567890");
+		cd.Y++;
+		SetConsoleCursorPosition(hOut, cd);
+		printf("S-DEBUG: 12345678901234567890");
+	}
+
+	SetConsoleCursorPosition(hOut, rend->statsarea_start);
+	int atk = plr->atkbase + plr->atkboost;
+	int def = plr->defbase + plr->defboost;
+	printf(" HP: %d\n", plr->health);
+	printf("Atk: %d   Def: %d", atk, def);
+	return 0;
+}
+
+static int renderer(System* sys, Renderer* rend, Player* plr) {
+	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	if (rend->upd_header)
+		rend->upd_header = rend_upd_header(sys, rend, hOut);
+	if (rend->upd_rendarea)
+		rend->upd_rendarea = rend_upd_gamearea(rend, hOut);
+	if (rend->upd_gamearea)
+		rend->upd_gamearea = upd_rend_gamearea_borders(rend, hOut);
+	if (rend->upd_stats)
+		rend->upd_stats = rend_upd_stats(sys, rend, plr, hOut);
 	return 0;
 }
 
@@ -79,8 +221,9 @@ int main(int argc, char* argv[]) {
 	Map* pmap = &m;
 	Player plr;
 	Player* pplr = &plr;
+	int ret_init = 0, ret_update = 0, ret_renderer = 0;
 	
-	int ret_init = init(psys, prend, pmap, pplr);
+	ret_init = init(psys, prend, pplr);
 
 	// THE MAIN LOOP
 	int cycles = 0;
@@ -91,22 +234,22 @@ int main(int argc, char* argv[]) {
 		else
 			cycles = 0;
 
-		printf("Cycles: %d\n", cycles);
+		// printf("Cycles: %d\n", cycles);
 
 
 		//input
 		switch (input()) {
 		case 'q': exit(0); break;
-		default: continue;
+		//default: continue;
 		}
 
 
 		//update
-			int ret_update = update();
+			ret_update = update();
 
 
 		//renderer
-			int ret_renderer = renderer();
+			ret_renderer = renderer(psys, prend, pplr);
 
 
 	} // END OF MAIN LOOP
--- DiggerTest2/main.h
+++ DiggerTest2/main.h
@@ -7,8 +7,8 @@
 
 #define REN_HEADER_ROWS 1
 #define REN_STATS_ROWS 2
-#define REN_ROWS 20
-#define REN_COLS 60
+#define REN_ROWS 22
+#define REN_COLS 78
 
 #define G_AREA_ROWS 40
 #define G_AREA_COLS 120
@@ -23,14 +23,15 @@
 #define CHAR_HEY 177
 #define CHAR_HEY_HIGH 178
 #define CHAR_ROCK 254
-#define CHAR_FRAME_UL    // Frame char of upper left corner
-#define CHAR_FRAME_UR    // Frame char of upper right corner
-#define CHAR_FRAME_UPPER // Frame char of upper horizontal line
-#define CHAR_FRAME_LL    // Frame char of lower left corner
-#define CHAR_FRAME_LR    // Frame char of lower right corner
-#define CHAR_FRAME_LOWER // Frame char of lower horizontal line
-#define CHAR_FRAME_L     // Frame char of left vertical line
-#define CHAR_FRAME_R     // Frame char of right vertical line
+#define CHAR_FRAME_UL 218   // Frame char of upper left corner
+#define CHAR_FRAME_UC 196   // Frame char of upper horizontal line
+#define CHAR_FRAME_UR 183   // Frame char of upper right corner
+#define CHAR_FRAME_LL 212   // Frame char of lower left corner
+#define CHAR_FRAME_LC 205   // Frame char of lower horizontal line
+#define CHAR_FRAME_LR 188   // Frame char of lower right corner
+#define CHAR_FRAME_L  179   // Frame char of left vertical line
+#define CHAR_FRAME_R  186   // Frame char of right vertical line
+
 
 
 
@@ -48,11 +49,38 @@ typedef struct block {
 
 Block blocks[] = {
 	{ CHAR_EMPTY, BL_WALKABLE },
+	{ CHAR_EMPTY, BL_WALKABLE },
+	{ CHAR_EMPTY, BL_WALKABLE },
+	{ CHAR_EMPTY, BL_WALKABLE },
+	{ CHAR_EMPTY, BL_WALKABLE },
+	{ CHAR_GRASS, BL_WALKABLE },
+	{ CHAR_GRASS, BL_WALKABLE },
 	{ CHAR_GRASS, BL_WALKABLE },
 	{ CHAR_HEY_LOW, BL_WALKABLE },
 	{ CHAR_HEY, BL_SLOWING },
 	{ CHAR_HEY_HIGH, BL_SLOWING },
 	{ CHAR_ROCK, BL_BLOCKING },
+};
+
+/* Wall chars
+0: UL
+1: UC
+2: UR
+3: LL
+4: LC
+5: LR
+6: L
+7: R
+*/
+Block walls[] = {
+	{ CHAR_FRAME_UL, BL_BLOCKING },
+	{ CHAR_FRAME_UC, BL_BLOCKING },
+	{ CHAR_FRAME_UR, BL_BLOCKING },
+	{ CHAR_FRAME_LL, BL_BLOCKING },
+	{ CHAR_FRAME_LC, BL_BLOCKING },
+	{ CHAR_FRAME_LR, BL_BLOCKING },
+	{ CHAR_FRAME_L, BL_BLOCKING },
+	{ CHAR_FRAME_R, BL_BLOCKING },
 };
 
 
@@ -63,13 +91,14 @@ typedef struct map {
 	const char tag;
 } Map;
 
-Map map[G_AREA_ROWS][G_AREA_COLS];
+Map gamemap[G_AREA_ROWS][G_AREA_COLS];
+Map rendmap[REN_ROWS][REN_COLS];
 
 
 typedef struct player {
-	unsigned char name; // Player name
-	COORD Position;     // Player current position
-	COORD PositionOld;  // Player old position
+	unsigned char name;   // Player name
+	COORD Position;       // Player current position
+	COORD PositionOld;    // Player old position
 	unsigned char dir;    // Player direction
 	unsigned char marker; // Player "icon"
 
@@ -95,12 +124,15 @@ typedef struct player {
 8: Toggle if stats area is updated
 */
 typedef struct renderer {
+	COORD rendarea_start;
+	COORD rendarea_end;
 	COORD header_start;
 	COORD header_end;
 	COORD gamearea_start;
 	COORD gamearea_end;
 	COORD statsarea_start;
 	COORD statsarea_end;
+	char upd_rendarea;
 	char upd_header;
 	char upd_gamearea;
 	char upd_stats;
